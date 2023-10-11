6f8ab61eafd601331ace8dd1d629add797d9840b
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-10-11 17:14:04 +0100

    sp

--- engine.Test/TokeniserUnitTests.fs
+++ engine.Test/TokeniserUnitTests.fs
@@ -9,11 +9,11 @@ type TokeniserTests () =
     [<Test>]
     member this._Test_Tokeniser_Pass() =
         let expected = [
-            engine.Tokeniser.createToken "Digit" '1';
-            engine.Tokeniser.createToken "Operator" '+';
-            engine.Tokeniser.createToken "Digit" '1'
+            engine.Tokeniser.CreateToken "Digit" '1';
+            engine.Tokeniser.CreateToken "Operator" '+';
+            engine.Tokeniser.CreateToken "Digit" '1'
         ]
-        let actual = engine.Tokeniser.tokenise "1+1"
+        let actual = engine.Tokeniser.Execute "1+1"
 
         Assert.AreEqual(expected |> List.toArray, actual |> List.toArray)
     
--- engine/Library.fs
+++ engine/Library.fs
@@ -7,45 +7,40 @@ module Tokeniser =
         Type: string;
         Value: char
     }
-    let createToken (tokenType: string) (value: char) : Token = {
+    let CreateToken (tokenType: string) (value: char) : Token = {
         Type = tokenType
         Value = value
      }
 
     // Helpers.
-    let isDigit char = 
+    let private isDigit char = 
         System.Char.IsDigit char
-    let isLetter char = 
+    let private isLetter char = 
         System.Char.IsLetter char
-    let isOperator c =
+    let private isOperator c =
         match c with
         | '+' | '-' | '*' | '/' -> true
         | _ -> false
-    let isLeftBracket char =  char = '('
-    let isRightBracket char = char = ')'
-    let isComma char = char = ','
-    let removeSpaces (str: string) =
+    let private isLeftBracket char =  char = '('
+    let private isRightBracket char = char = ')'
+    let private isComma char = char = ','
+    let private removeSpaces (str: string) =
            str.Replace(" ", "")
 
     // Decide type.
-    let categorizeChar char =
+    let private categorizeChar char =
         match char with
-        | _ when isDigit char             -> createToken "Digit" char
-        | _ when isLetter char            -> createToken "Letter" char
-        | _ when isOperator char          -> createToken "Operator" char
-        | _ when isLeftBracket char       -> createToken "LeftBracket" char
-        | _ when isRightBracket char      -> createToken "RightBracket" char
-        | _ when isComma char             -> createToken "Comma" char
-        | _                               -> createToken "Unknown" char
+        | _ when isDigit char             -> CreateToken "Digit" char
+        | _ when isLetter char            -> CreateToken "Letter" char
+        | _ when isOperator char          -> CreateToken "Operator" char
+        | _ when isLeftBracket char       -> CreateToken "LeftBracket" char
+        | _ when isRightBracket char      -> CreateToken "RightBracket" char
+        | _ when isComma char             -> CreateToken "Comma" char
+        | _                               -> CreateToken "Unknown" char
 
     // Tokenise.
-    let tokenise str =
+    let Execute str =
         let noSpaceStr = removeSpaces str
 
         // Run through the expression and split into tokens, returns list.
         [for char in noSpaceStr do yield categorizeChar char]
-
-    // Test(for now).
-    //let results = tokenise "11 + sin(20.4)"
-    //for item in results do
-    //    printfn "Type: %s, Value: %c" item.Type item.Value
