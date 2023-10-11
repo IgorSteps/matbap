9e8ca52b6e251e00f04f05fbe802b5d6d4f97f6d
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-10-11 21:18:57 +0100

    Add table driven tests

--- engine.Test/TokeniserUnitTests.fs
+++ engine.Test/TokeniserUnitTests.fs
@@ -2,21 +2,46 @@
 
 open NUnit.Framework
 
+type TokeniserTestCase = {
+    Args: string;
+    Expected: Engine.Tokeniser.Token list
+}
+
 [<TestFixture>]
 type TokeniserTests () =
+    static member testCases = [
+       {
+            Args = "1a+12";
+            Expected = [
+               Engine.Tokeniser.createToken Engine.Tokeniser.Digit '1';
+               Engine.Tokeniser.createToken Engine.Tokeniser.Letter 'a';
+               Engine.Tokeniser.createToken Engine.Tokeniser.Operator '+';
+               Engine.Tokeniser.createToken Engine.Tokeniser.Digit '1'
+               Engine.Tokeniser.createToken Engine.Tokeniser.Digit '2'
+            ]
+       };
+       {
+            Args = "1a+(4b)";
+            Expected = [
+                 Engine.Tokeniser.createToken Engine.Tokeniser.Digit '1';
+                 Engine.Tokeniser.createToken Engine.Tokeniser.Letter 'a';
+                 Engine.Tokeniser.createToken Engine.Tokeniser.Operator '+';
+                 Engine.Tokeniser.createToken Engine.Tokeniser.LeftBracket '(';
+                 Engine.Tokeniser.createToken Engine.Tokeniser.Digit '4'
+                 Engine.Tokeniser.createToken Engine.Tokeniser.Letter 'b'
+                 Engine.Tokeniser.createToken Engine.Tokeniser.RightBracket ')';
+            ]
+       }
+    ]
 
-    [<Test>]
-    member this._Test_Tokeniser_Pass() =
+    [<TestCaseSource("testCases")>]
+    member this._Test_Tokeniser_Pass(testCase: TokeniserTestCase) =
         // --------
         // Assemble
         // --------
-        let expected = [
-            Engine.Tokeniser.createToken Engine.Tokeniser.Digit '1';
-            Engine.Tokeniser.createToken Engine.Tokeniser.Operator '+';
-            Engine.Tokeniser.createToken Engine.Tokeniser.Digit '1'
-        ]
-        let args = "1+1"
-
+        let args = testCase.Args
+        let expected = testCase.Expected
+  
         // ---
         // Act
         // ---
@@ -27,4 +52,4 @@ type TokeniserTests () =
         // ------
         match actual with
         | Ok tokenList -> Assert.AreEqual(expected |> List.toArray, tokenList |> List.toArray)
-        | Error errMsg -> failwithf "failed to tokenise: %s" errMsg
+        | Error errMsg -> failwithf "Unexpected failure: failed to tokenise: %s" errMsg
