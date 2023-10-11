68fb3e02aabd62046756d02ee6745f1abda3011b
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-10-11 12:51:51 +0100

    cleanup, extra comments

--- engine/Library.fs
+++ engine/Library.fs
@@ -7,6 +7,10 @@ module Tokeniser =
         Type: string;
         Value: char
     }
+    let createToken (tokenType: string) (value: char) : Token = {
+        Type = tokenType
+        Value = value
+     }
 
     // Helpers.
     let isDigit char = 
@@ -17,8 +21,8 @@ module Tokeniser =
         match c with
         | '+' | '-' | '*' | '/' -> true
         | _ -> false
-    let isLeftParenthesis char =  char = '('
-    let isRightParenthesis char = char = ')'
+    let isLeftBracket char =  char = '('
+    let isRightBracket char = char = ')'
     let isComma char = char = ','
     let removeSpaces (str: string) =
            str.Replace(" ", "")
@@ -26,27 +30,22 @@ module Tokeniser =
     // Decide type.
     let categorizeChar char =
         match char with
-        | _ when isDigit char             -> { Type = "Literal"; Value = char }
-        | _ when isLetter char            -> { Type = "Variable"; Value = char }
-        | _ when isOperator char          -> { Type = "Operator"; Value = char }
-        | _ when isLeftParenthesis char   -> { Type = "LeftParenthesis"; Value = char }
-        | _ when isRightParenthesis char  -> { Type = "RightParenthesis"; Value = char }
-        | _ when isComma char             -> { Type = "FunctionArgumentSeparator"; Value = char }
-        | _                               -> { Type = "Unknown"; Value = char }
+        | _ when isDigit char             -> createToken "Digit" char
+        | _ when isLetter char            -> createToken "Letter" char
+        | _ when isOperator char          -> createToken "Operator" char
+        | _ when isLeftBracket char       -> createToken "LeftBracket" char
+        | _ when isRightBracket char      -> createToken "RightBracket" char
+        | _ when isComma char             -> createToken "Comma" char
+        | _                               -> createToken "Unknown" char
 
     // Tokenise.
     let tokenise str =
         let noSpaceStr = removeSpaces str
-        
-        [for char in noSpaceStr do
-            yield
-                categorizeChar char]
+
+        // Run through the expression and split into tokens, returns list.
+        [for char in noSpaceStr do yield categorizeChar char]
 
     // Test(for now).
-    let results = tokenise "a+1,(b-2)"
+    let results = tokenise "11 + sin(20.4)"
     for item in results do
         printfn "Type: %s, Value: %c" item.Type item.Value
-    
-    
-    
-        
