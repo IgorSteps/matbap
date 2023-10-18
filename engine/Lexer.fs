89dccde0bd37f21b3e1628395b4be335b1fb87e4
LiamFarese <108936972+LiamFarese@users.noreply.github.com>
2023-10-18 22:14:09 +0100

    Changed from throwing an exception to returning error codes with Result, tokenize changed to a tail recursive function

--- engine/Lexer.fs
+++ engine/Lexer.fs
@@ -1,6 +1,10 @@
 ﻿namespace Engine
     module Tokeniser =
 
+        type LexicalError =
+            | InvalidFloat
+            | InvalidToken
+
         type TokenType =
             | Variable of string
             | Int of int
@@ -12,6 +16,7 @@
             | LeftBracket
             | RightBracket
             | Modulus
+            | Equals
             | Sin
             | Cos
             | Tan
@@ -22,64 +27,75 @@
         let strtochar (str: string) : char list = [for c in str do yield c]
         let isDigit(c: char)         = System.Char.IsDigit c
         let isLetter(c: char)        = System.Char.IsLetter c
-        let isComma (c: char)        = c = ','
         let noStartingSpace(c: char) = if c <> ' ' then true else false
         let isKeyword (str: string)  = if keywords.ContainsKey str then true else false
 
-        let rec isFloat(chars: char list): bool =
+        //returns an error if the float is formatted wrong
+        let isFloat(chars: char list) : Result<unit, LexicalError> =
 
-            let firstChar = if noStartingSpace chars.Head && not (isLetter chars.Head) then
-                                true 
-                            else
-                                failwith "Invalid float"
+            let firstCharValid = if noStartingSpace chars.Head && not (isLetter chars.Head) then
+                                    Ok ()
+                                 else
+                                    Error InvalidFloat
 
             let rec postFirstChar chars =
                 match chars with
                     | c::tail when isDigit c -> postFirstChar tail
-                    | '.'::_                 -> false
-                    | _                      -> true
-    
-            postFirstChar chars
-        
+                    | '.'::_                 -> Error InvalidFloat
+                    | _                      -> Ok ()
+
+            match firstCharValid with
+            | Ok ()     -> postFirstChar chars
+            | Error err -> Error err
+
+        //returns a Result as it may error if the number is an incorrectly formatted string
         let rec formFractionalPart(chars: char list, accStr: string) =
             match chars with
             | c::tail when isDigit c -> formFractionalPart(tail, accStr + string c)
-            | _                      -> (chars, accStr, "float")
-
+            | _                      -> Ok (chars, accStr, "float")
+        
+        //returns a Result as it may error if the number is an incorrectly formatted string
         let rec formInt(chars: char list, accStr: string) = 
             match chars with
             | c :: tail when isDigit c    -> formInt(tail, accStr + string c)
-            | '.'::tail when isFloat tail -> formFractionalPart(tail, accStr + string '.')
-            | _                           -> (chars, accStr, "int")
+            | '.'::tail                   -> match isFloat tail with
+                                             | Ok ()     -> formFractionalPart(tail, accStr + string '.')
+                                             | Error err -> Error err
+            | _                           -> Ok (chars, accStr, "int")
 
         let rec formString(chars: char list, accStr: string) =
             match chars with
             | c::tail when isLetter c -> formString(tail, accStr + string c)
             | c::tail when isDigit c  -> formString(tail, accStr + string c)
             | _                       -> (chars, accStr)
-
-        let tokenize (str : string) : TokenType list=
-            let rec matchTokens chars =
+        
+        //tail recursive because it has to be wrapped in a result
+        let tokenize (str : string): Result<TokenType list, LexicalError> =
+            let rec matchTokens chars acc =
                 match chars with 
-                | []          -> []
-                | '+' :: tail -> Add         :: matchTokens tail
-                | '-' :: tail -> Minus       :: matchTokens tail
-                | '*' :: tail -> Multiply    :: matchTokens tail
-                | '/' :: tail -> Divide      :: matchTokens tail
-                | '(' :: tail -> LeftBracket :: matchTokens tail
-                | ')' :: tail -> RightBracket:: matchTokens tail
-                | '%' :: tail -> Modulus     :: matchTokens tail
-                | ' ' :: tail ->                matchTokens tail
-                | head :: tail when isDigit head  -> let (chars, numString, tokenType) = 
-                                                        formInt(tail, string head)
-                                                     if tokenType = "int" then
-                                                        Int (int numString)  :: matchTokens chars
-                                                     else
-                                                        Float (float numString):: matchTokens chars
-                | head :: tail when isLetter head -> let (chars, var) = formString(tail, string head)
-                                                     if isKeyword var then 
-                                                        keywords[var]:: matchTokens chars
-                                                     else 
-                                                        Variable var :: matchTokens chars
-                | _ -> failwith "Invalid Token"
-            matchTokens (strtochar str)
+                | []          -> Ok(List.rev acc)
+                | '+' :: tail -> matchTokens tail (Add::acc)
+                | '-' :: tail -> matchTokens tail (Minus::acc)
+                | '*' :: tail -> matchTokens tail (Multiply::acc)
+                | '/' :: tail -> matchTokens tail (Divide::acc)
+                | '(' :: tail -> matchTokens tail (LeftBracket::acc)
+                | ')' :: tail -> matchTokens tail (RightBracket::acc)
+                | '%' :: tail -> matchTokens tail (Modulus::acc)
+                | '=' :: tail -> matchTokens tail (Equals::acc)
+                | ' ' :: tail -> matchTokens tail acc
+                | head :: tail when isDigit head ->
+                    match formInt(tail, string head) with
+                    | Ok (chars, numString, tokenType) ->
+                        if tokenType = "int" then
+                            matchTokens chars (Int (int numString)::acc)
+                        else
+                            matchTokens chars (Float (float numString)::acc)
+                    | Error err -> Error err
+                | head :: tail when isLetter head ->
+                    let (chars, var) = formString(tail, string head)
+                    if isKeyword var then 
+                        matchTokens chars (keywords[var]::acc)
+                    else 
+                        matchTokens chars (Variable var::acc)
+                | _ -> Error InvalidToken
+            matchTokens(strtochar str) []
