3663399f33a4b270dfdcca24bd58e421951c1833
LiamFarese <108936972+LiamFarese@users.noreply.github.com>
2023-10-18 04:13:44 +0100

    seperated out Number into int and float, added modulus recognition

--- engine/Lexer.fs
+++ engine/Lexer.fs
@@ -2,33 +2,24 @@
     module Tokeniser =
 
         type TokenType =
-            | Variable
-            | Plus
+            | Variable of string
+            | Int of int
+            | Float of float
+            | Add
             | Minus
             | Multiply
             | Divide
             | LeftBracket
             | RightBracket
-            | Number
+            | Modulus
             | Sin
             | Cos
             | Tan
 
         let keywords = Map.empty.Add("sin", Sin).Add("cos", Cos).Add("cos", Cos)
 
-        type Token = {
-            Type: TokenType
-            Value: string option //not all tokens have a value so it's optional
-        }
-
-        let createToken(tokenType: TokenType, value: string option) : Token = {
-            Type = tokenType
-            Value = value
-        }
-
         // Helpers.
         let strtochar (str: string) : char list = [for c in str do yield c]
-
         let isDigit(c: char)         = System.Char.IsDigit c
         let isLetter(c: char)        = System.Char.IsLetter c
         let isComma (c: char)        = c = ','
@@ -49,12 +40,17 @@
                     | _                      -> true
     
             postFirstChar chars
-
-        let rec formNum(chars: char list, accStr: string) = 
+        
+        let rec formFractionalPart(chars: char list, accStr: string) =
             match chars with
-            | c :: tail when isDigit c    -> formNum(tail, accStr + string c)
-            | '.'::tail when isFloat tail -> formNum(tail, accStr + string '.')
-            | _                           -> (chars, accStr)
+            | c::tail when isDigit c -> formFractionalPart(tail, accStr + string c)
+            | _                      -> (chars, accStr, "float")
+
+        let rec formInt(chars: char list, accStr: string) = 
+            match chars with
+            | c :: tail when isDigit c    -> formInt(tail, accStr + string c)
+            | '.'::tail when isFloat tail -> formFractionalPart(tail, accStr + string '.')
+            | _                           -> (chars, accStr, "int")
 
         let rec formString(chars: char list, accStr: string) =
             match chars with
@@ -62,26 +58,28 @@
             | c::tail when isDigit c  -> formString(tail, accStr + string c)
             | _                       -> (chars, accStr)
 
-        let tokenize (str : string) : Token list =
+        let tokenize (str : string) : TokenType list=
             let rec matchTokens chars =
                 match chars with 
                 | []          -> []
-                | '+' :: tail -> createToken(Plus, None)        :: matchTokens tail
-                | '-' :: tail -> createToken(Minus, None)       :: matchTokens tail
-                | '*' :: tail -> createToken(Multiply, None)    :: matchTokens tail
-                | '/' :: tail -> createToken(Divide, None)      :: matchTokens tail
-                | '(' :: tail -> createToken(LeftBracket, None) :: matchTokens tail
-                | ')' :: tail -> createToken(RightBracket, None):: matchTokens tail
-                | ' ' :: tail ->                                   matchTokens tail
-                | head :: tail when isDigit head  -> let (chars, numString) = formNum(tail, string head)
-                                                     createToken (Number, Some numString) 
-                                                     :: matchTokens chars
+                | '+' :: tail -> Add         :: matchTokens tail
+                | '-' :: tail -> Minus       :: matchTokens tail
+                | '*' :: tail -> Multiply    :: matchTokens tail
+                | '/' :: tail -> Divide      :: matchTokens tail
+                | '(' :: tail -> LeftBracket :: matchTokens tail
+                | ')' :: tail -> RightBracket:: matchTokens tail
+                | '%' :: tail -> Modulus     :: matchTokens tail
+                | ' ' :: tail ->                matchTokens tail
+                | head :: tail when isDigit head  -> let (chars, numString, tokenType) = 
+                                                        formInt(tail, string head)
+                                                     if tokenType = "int" then
+                                                        Int (int numString)  :: matchTokens chars
+                                                     else
+                                                        Float (float numString):: matchTokens chars
                 | head :: tail when isLetter head -> let (chars, var) = formString(tail, string head)
                                                      if isKeyword var then 
-                                                        createToken(keywords[var], None)
-                                                        :: matchTokens chars
+                                                        keywords[var]:: matchTokens chars
                                                      else 
-                                                        createToken (Variable, Some var) 
-                                                        :: matchTokens chars
+                                                        Variable var :: matchTokens chars
                 | _ -> failwith "Invalid Token"
             matchTokens (strtochar str)
