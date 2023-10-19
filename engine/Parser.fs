9da9d0e05c8ed58c86c680a38ebaf5b5fb159b3d
Lyra <118722990+lyra-scarlet@users.noreply.github.com>
2023-10-19 10:14:52 +0100

    Parser rewrite

--- engine/Parser.fs
+++ engine/Parser.fs
@@ -1,71 +1,36 @@
 ﻿namespace Engine
     module Parser =
-        // Current grammar
-        // <expr> ::= <term>
-        // | <expr> + <term>
-        // | <expr> - <term>
-        // <term> ::= <factor>
-        // | <term> * <factor>
-        // | <term> / <factor>
-        // <factor> ::= <float> | (<expr>)
+        // Grammar for rewrite
+        // <E>    ::= <T> <Eopt>
+        // <Eopt> ::= + <T> <Eopt> | - <T> <Eopt> | <empty>
+        // <T>    ::= <NR> <Topt>
+        // <Topt> ::= * <NR> <Topt> | / <NR> <Topt> | <empty>
+        // <NR>   ::= Num <value> | (E)
         
-        type Exp =
-            | Term of Term
-            | Add of Exp * Term
-            | Subtract of Exp * Term
-        and Term =
-            | Factor of Factor
-            | Multiply of Term * Factor
-            | Divide of Term * Factor
-        and Factor =
-            | Float of float
-            | Int of int
-            | Exp of Exp
- 
-        // Recursive parse functions
-        let rec parseExp tokens =
-            match tokens with
-            | [] -> failwith "Input ended unexpectedly"
-            | Tokeniser.Add :: restTokens ->
-                let term, remainingTokens = parseTerm restTokens
-                let exp, finalTokens = parseExp remainingTokens
-                Add(exp, term), finalTokens
-            | Tokeniser.Minus :: restTokens ->
-                let term, remainingTokens = parseTerm restTokens
-                let exp, finalTokens = parseExp remainingTokens
-                Subtract(exp, term), finalTokens
-            | _ :: restTokens ->
-                let term, finalTokens = parseTerm restTokens
-                Term term, finalTokens
-        and parseTerm tokens =
-            match tokens with
-            | [] -> failwith "Input ended unexpectedly"
-            | Tokeniser.Multiply :: restTokens ->
-                let factor, remainingTokens = parseFactor restTokens
-                let term, finalTokens = parseTerm remainingTokens
-                Multiply(term, factor), finalTokens
-            | Tokeniser.Divide :: restTokens ->
-                let factor, remainingTokens = parseFactor restTokens
-                let term, finalTokens = parseTerm remainingTokens
-                Divide(term, factor), finalTokens
-            | _ :: restTokens ->
-                let factor, finalTokens = parseFactor restTokens
-                Factor factor, finalTokens
-        and parseFactor tokens =
-            match tokens with
-            | [] -> failwith "Input ended unexpectedly"
-            | Tokeniser.Int(int) :: restTokens -> Int(int), restTokens
-            | Tokeniser.Float(float) :: restTokens -> Float(float), restTokens
-            | Tokeniser.LeftBracket :: restTokens ->
-                let exp, remainingTokens = parseExp restTokens
-                match remainingTokens with
-                | Tokeniser.RightBracket :: finalTokens -> Exp(exp), finalTokens
-                | _ -> failwith "Could not find closing parenthesis"
-            | _ -> failwith "Found an invalid factor"
-
-        let parse input =
-            let ast, remainingTokens = parseExp input
-            if remainingTokens = [] then
-                Some ast
-            else
-                None
+        let parseError = System.Exception("Parse error")
+        let parse tList =
+            // Doesn't return anything but does parse
+            let rec E tList = (T >> Eopt) tList
+            and Eopt tList =
+                match tList with
+                | Tokeniser.Add :: tail -> (T >> Eopt) tail
+                | Tokeniser.Minus :: tail -> (T >> Eopt) tail
+                | _ -> tList
+            and T tList = (NR >> Topt) tList
+            and Topt tList =
+                match tList with
+                | Tokeniser.Multiply :: tail -> (NR >> Topt) tail
+                | Tokeniser.Divide :: tail -> (NR >> Topt) tail
+                | _ -> tList
+            and NR tList =
+                match tList with
+                | Tokeniser.Int value :: tail -> tail
+                | Tokeniser.Float value :: tail -> tail
+                // Parser raises an error. Will make this method private and try/catch it
+                | Tokeniser.LeftBracket :: tail -> match E tail with
+                                                   | Tokeniser.RightBracket :: tail -> tail
+                                                   | _ -> raise parseError
+                | _ -> raise parseError
+            E tList
+        
+        
