1407b120cf739bb56d27d6530ab2b414f79a748d
LiamFarese <108936972+LiamFarese@users.noreply.github.com>
2023-10-19 17:02:51 +0100

    Introduced NumberToken D Union

--- engine/Lexer.fs
+++ engine/Lexer.fs
@@ -5,10 +5,13 @@
             | InvalidFloat
             | InvalidToken
 
+        type NumberToken =
+            | IntToken of int
+            | FloatToken of float
+
         type Token =
             | Variable of string
-            | Int of int
-            | Float of float
+            | NumberToken of NumberToken
             | Add
             | Minus
             | Multiply
@@ -16,25 +19,28 @@
             | LeftBracket
             | RightBracket
             | Modulus
+            | Power
             | Equals
             | Sin
             | Cos
             | Tan
 
-        //map stores reserved keywords, used to replace string variables
-        //with the corresponding enum type when encountered in the tokenizer function
-        let private keywords = Map.empty.Add("sin", Sin).Add("cos", Cos).Add("cos", Cos)
+        // Map stores reserved keywords, used to replace string variables
+        // with the corresponding enum type when encountered in the tokenizer function
+        let private keywords = Map.empty.Add("sin", Sin).Add("cos", Cos).Add("tan", Tan)
 
         // Helpers.
-        let private strtochar (str: string) : char list = [for c in str do yield c]
+        let private strTochar(str: string) : char list = [for c in str do yield c]
+        let private charToInt(char: char) = int char - int '0'
+
         let private isDigit(c: char)         = System.Char.IsDigit c
         let private isLetter(c: char)        = System.Char.IsLetter c
-        let private noStartingSpace(c: char) = if c <> ' ' then true else false
-        let private isKeyword (str: string)  = if keywords.ContainsKey str then true else false
+        let private noStartingSpace(c: char) = c <> ' '
+        let private isKeyword(str: string)   = keywords.ContainsKey str
 
-        //takes fractional part of a float and checks that it doesn't lead with a space or character
-        //then checks that no aditional decimal points are among the digits,
-        //returns no error if it passed both checks, else it returns an InvalidFloat LexicalError
+        // Takes fractional part of a float and checks that it doesn't lead with a space or character
+        // then checks that no aditional decimal points are among the digits,
+        // returns no error if it passed both checks, else it returns an InvalidFloat LexicalError
         let private isFloat(chars: char list) : Result<unit, LexicalError> =
 
             let firstCharValid = if noStartingSpace chars.Head && not (isLetter chars.Head) then
@@ -52,34 +58,29 @@
             | Ok ()     -> postFirstChar chars
             | Error err -> Error err
 
-        //formFractionalPart and formInt take in the char list and a variable 'accStr' 
-        //(accumulated String) used to store resulting number in string form, 
-        //both functions return Result<(chars, accStr, tokenType), LexicalError> where 
-        //chars = remaining char list,
-        //accStr = number in string form
-        //tokenType = string denoting the type of accStr (int or float)
-        let rec private formFractionalPart(chars: char list, accStr: string) =
+        let rec private formFractionalPart(chars: char list, acc: float) =
             match chars with
-            | c::tail when isDigit c -> formFractionalPart(tail, accStr + string c)
-            | _                      -> Ok (chars, accStr, "float")
+            | c::tail when isDigit c -> formFractionalPart(tail, acc + float ((charToInt c)/10))
+            | _                      -> Ok (chars, FloatToken acc)
         
         
-        let rec private formInt(chars: char list, accStr: string) = 
+        let rec private formInt(chars: char list, acc: int) =
             match chars with
-            | c :: tail when isDigit c -> formInt(tail, accStr + string c)
+            | c :: tail when isDigit c -> formInt(tail, (acc*10) + charToInt c)
             | '.'::tail                -> match isFloat tail with
-                                          | Ok ()     -> formFractionalPart(tail, accStr + string '.')
+                                          | Ok ()     -> formFractionalPart(tail, float acc)
+                                            
                                           | Error err -> Error err
-            | _                        -> Ok (chars, accStr, "int")
+            | _                        -> Ok (chars, IntToken acc)
 
-        let rec private formString(chars: char list, accStr: string) =
+        let rec private formVariable(chars: char list, accStr: string) =
             match chars with
-            | c::tail when isLetter c -> formString(tail, accStr + string c)
-            | c::tail when isDigit c  -> formString(tail, accStr + string c)
+            | c::tail when isLetter c -> formVariable(tail, accStr + string c)
+            | c::tail when isDigit c  -> formVariable(tail, accStr + string c)
             | _                       -> (chars, accStr)
         
-        //tail recursive because it has to be wrapped in a result
-        let tokenise (str : string): Result<Token list, LexicalError> =
+        // Tail recursive because it has to be wrapped in a result
+        let tokenise(str : string): Result<Token list, LexicalError> =
             let rec matchTokens chars acc =
                 match chars with 
                 | []          -> Ok(List.rev acc)
@@ -90,24 +91,21 @@
                 | '(' :: tail -> matchTokens tail (LeftBracket::acc)
                 | ')' :: tail -> matchTokens tail (RightBracket::acc)
                 | '%' :: tail -> matchTokens tail (Modulus::acc)
+                | '^' :: tail -> matchTokens tail (Power::acc)
                 | '=' :: tail -> matchTokens tail (Equals::acc)
                 | ' ' :: tail -> matchTokens tail acc
 
                 | head :: tail when isDigit head ->
-                    match formInt(tail, string head) with
-                    | Ok (chars, numString, tokenType) ->
-                        if tokenType = "int" then
-                            matchTokens chars (Int (int numString)::acc)
-                        else
-                            matchTokens chars (Float (float numString)::acc)
+                    match formInt(tail, int head) with
+                    | Ok (chars, num) -> matchTokens chars ( NumberToken num::acc)
                     | Error err -> Error err
 
                 | head :: tail when isLetter head ->
-                    let (chars, var) = formString(tail, string head)
+                    let (chars, var) = formVariable(tail, string head)
                     if isKeyword var then 
                         matchTokens chars (keywords[var]::acc)
                     else 
                         matchTokens chars (Variable var::acc)
                 | _ -> Error InvalidToken
 
-            matchTokens(strtochar str) []
+            matchTokens(strTochar str) []
