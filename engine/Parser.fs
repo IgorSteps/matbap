0ec714d60e4357f9a73bcd70490f2a3aa07d6207
Lyra <118722990+lyra-scarlet@users.noreply.github.com>
2023-10-20 13:07:56 +0100

    Parse & evaluate function

--- engine/Parser.fs
+++ engine/Parser.fs
@@ -6,7 +6,13 @@
         // <T>    ::= <NR> <Topt>
         // <Topt> ::= * <NR> <Topt> | / <NR> <Topt> | <empty>
         // <NR>   ::= Num <value> | (E)
-        
+        type Ast =
+            | Ident of string
+            | Val of System.Double
+            | Multi of Ast * Ast
+            | Div of Ast * Ast
+            | Plus of Ast * Ast
+            | Minus of Ast * Ast
         let parseError = System.Exception("Parse error")
         let parse tList =
             // Doesn't return anything but does parse
@@ -33,4 +39,30 @@
                 | _ -> raise parseError
             E tList
         
-        
+        let parseEval tList =
+            let rec E tList = (T >> Eopt) tList
+            and Eopt (tList, inval) =
+                match tList with
+                    | Tokeniser.Add :: tail ->      let (ts1, Tvalue) = T tail
+                                                    Eopt (ts1, inval+Tvalue)
+                    | Tokeniser.Minus :: tail ->    let (ts1, Tvalue) = T tail
+                                                    Eopt (ts1, inval+Tvalue)
+                    | _ -> (tList, inval)
+            and T tList = ( NR >> Topt ) tList
+            and Topt (tList, inval) =
+                match tList with
+                | Tokeniser.Multiply :: tail -> let (ts1, NRvalue) = NR tail
+                                                Topt (ts1, inval*NRvalue)
+                | Tokeniser.Divide :: tail ->   let (ts1, NRvalue) = NR tail
+                                                Topt (ts1, inval/NRvalue)
+                | _ -> (tList, inval)
+            and NR tList =
+                match tList with
+                | Tokeniser.Float value :: tail ->  (tail, value)
+                | Tokeniser.Int value :: tail ->    (tail, value)
+                | Tokeniser.LeftBracket :: tail ->  let (ts1, Evalue) = E tail
+                                                    match ts1 with
+                                                    | Tokeniser.RightBracket :: tail -> (tail, Evalue)
+                                                    | _ -> raise parseError
+                | _ -> raise parseError
+            E tList
