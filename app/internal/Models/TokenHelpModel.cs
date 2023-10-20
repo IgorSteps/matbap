76071e28650772e1e07c1326d7f807eedcbb704b
Igor Stepanenko <igorstepanenko696@gmail.com>
2023-10-20 11:16:52 +0100

    Started implementation

---
+++ app/internal/Models/TokenHelpModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace app
+{
+	public class TokenHelp
+	{
+		private char _token;
+
+		public TokenHelp(char token)
+		{
+			_token = token;
+		}
+
+		public string Token
+		{
+			get;
+			set;
+		}
+	}
+}
+
---
+++ app/internal/ViewModels/TokenHelpViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace app
+{
+	public class TokenHelpViewModel : ObservableObject
+    {
+        private List<TokenHelp> _supportedTokens { get; set; }
+
+        public TokenHelpViewModel()
+		{
+            _supportedTokens = new List<TokenHelp>
+            {
+                new TokenHelp('+'),
+                // TODO other tokens
+            };
+        }
+	}
+}
+
