bc07ee57c8f489d2704be820e63d9c6e5084a3fd
IgorSteps <igorstepanenko696@gmail.com>
2023-11-16 20:15:23 +0000

    Add example unit test for future

--- app.Test/PlotViewModelUnitTest.cs
+++ app.Test/PlotViewModelUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿
+using Moq;
 using OxyPlot.Series;
 using System.Collections.ObjectModel;
 
@@ -6,6 +7,37 @@ namespace app.Test
 {
     internal class PlotViewModelUnitTest
     {
-      
+        private app.PlotViewModel _viewModel;
+        private Mock<IPlotEquationEvaluator> _modelMock;
+
+        [SetUp]
+        public void Setup()
+        {
+            _modelMock = new Mock<IPlotEquationEvaluator>();
+            _viewModel = new PlotViewModel(_modelMock.Object);
+        }
+
+        // Test stub for future.
+        [Test]
+        public void PlotViewModel_InterpretCommand_WillDoSomething()
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            var testInput = "y=2x+12";
+            var expectedResponse = "123";
+            _viewModel.InputEquation = testInput;
+            _modelMock.Setup(i => i.Evaluate(testInput)).Returns(expectedResponse);
+
+            // ---
+            // ACT
+            // ---
+            _viewModel.InterpretCmd.Execute(null);
+
+            // ------
+            // ASSERT
+            // ------
+            //Assert.That(_viewModel.Response, Is.EqualTo(expectedResponse), "Actual response is not equal expected");
+        }
     }
 }
