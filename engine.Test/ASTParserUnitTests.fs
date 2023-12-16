98e45fe2582088e52731cb7e8f55f953ff1488d9
Igor Stepanenko <84919282+IgorSteps@users.noreply.github.com>
2023-12-16 23:14:54 +0000

    AP-82: Add AST parser for basic grammar. (#35)
    * Add basic AST parser and unit tests
    
    * Cleanup
    
    * Better naming

---
+++ engine.Test/ASTParserUnitTests.fs
@@ -0,0 +1,117 @@
+﻿namespace Engine.Tests
+
+open Engine
+open Engine.Types
+open Engine.ASTParser
+open NUnit.Framework
+open System.Collections.Generic
+
+type ASTParserTestCase = {
+    Name: string
+    Args: Tokeniser.Token list;
+    Expected: Result<Node, string>
+}
+
+
+[<TestFixture>]
+type ASTParserTests() =
+    static member AstParserTestCases: ASTParserTestCase list = [
+        // Float vs Int difference.
+        {
+            Name = "Test parser differentiates floats and ints."
+            Args = [Tokeniser.Int 2]
+            Expected = Ok (Number (Int 2))
+        }
+        {
+            Name = "Test parser differentiates floats and ints."
+            Args = [Tokeniser.Float 2.5]
+            Expected = Ok (Number (Float 2.5))
+        }
+        // Simple arithemtic operations.
+        {
+            Name = "Test simple addition: 1+1."
+            Args = [Tokeniser.Int 1; Tokeniser.Add; Tokeniser.Int 1]
+            Expected = Ok (BinaryOperation ("+", Number (Int 1), Number (Int 1)))
+        }
+        {
+            Name = "Test simple substraction: 2-1."
+            Args = [Tokeniser.Int 2; Tokeniser.Minus; Tokeniser.Int 1]
+            Expected = Ok (BinaryOperation ("-", Number (Int 2), Number (Int 1)))
+        }
+        {
+            Name = "Test simple mutliplication: 5*5."
+            Args = [Tokeniser.Int 5; Tokeniser.Multiply; Tokeniser.Int 5]
+            Expected = Ok (BinaryOperation ("*", Number (Int 5), Number (Int 5)))
+        }
+        {
+            Name = "Test simple division: 10/5."
+            Args = [Tokeniser.Int 10; Tokeniser.Divide; Tokeniser.Int 5]
+            Expected = Ok (BinaryOperation ("/", Number (Int 10), Number (Int 5)))
+        }
+        // Combined arithemtic operations.
+        {
+            Name = "Test combined operations: 1+2/3."
+            Args = [Tokeniser.Int 1; Tokeniser.Add; Tokeniser.Int 2; Tokeniser.Divide; Tokeniser.Int 3]
+            Expected = Ok (BinaryOperation ("+", Number (Int 1), BinaryOperation ("/", Number (Int 2), Number (Int 3))))
+        }
+        {
+            Name = "Test combined operations: 4*2 +3."
+            Args = [Tokeniser.Int 4; Tokeniser.Multiply; Tokeniser.Int 2; Tokeniser.Add; Tokeniser.Int 3]
+            Expected = Ok (BinaryOperation ("+", BinaryOperation ("*", Number (Int 4), Number (Int 2)), Number (Int 3)))
+        }
+        // Parenthesised expressions.
+        {
+            Name = "Test parenthesised expression: (1+2)*3."
+            Args = [Tokeniser.LeftBracket; Tokeniser.Int 1; Tokeniser.Add; Tokeniser.Int 2; Tokeniser.RightBracket; Tokeniser.Multiply; Tokeniser.Int 3]
+            Expected = Ok (BinaryOperation ("*", ParenthesisExpression (BinaryOperation ("+", Number (Int 1), Number (Int 2))), Number (Int 3)))
+        }
+        {
+            Name = "Test parenthesised expression: 4*(2+3)."
+            Args = [Tokeniser.Int 4; Tokeniser.Multiply; Tokeniser.LeftBracket; Tokeniser.Int 2; Tokeniser.Add; Tokeniser.Int 3; Tokeniser.RightBracket;]
+            Expected = Ok (BinaryOperation ("*", Number (Int 4), ParenthesisExpression (BinaryOperation ("+", Number (Int 2), Number (Int 3)))))
+        }
+        // Nested parentheses.
+        {
+            Name = "Test nested parentheses: ((1+2)*3)/4"
+            Args = [Tokeniser.LeftBracket; Tokeniser.LeftBracket; Tokeniser.Int 1; Tokeniser.Add;
+                Tokeniser.Int 2; Tokeniser.RightBracket; Tokeniser.Multiply; Tokeniser.Int 3; Tokeniser.RightBracket; Tokeniser.Divide; Tokeniser.Int 4]
+            Expected = Ok (BinaryOperation ("/", ParenthesisExpression (BinaryOperation ("*", ParenthesisExpression (BinaryOperation ("+", Number (Int 1), Number (Int 2))), Number (Int 3))), Number (Int 4)))
+        }
+        // Errors.
+        {
+            Name = "Testing error: Missing a number or a bracket: 1+."
+            Args = [Tokeniser.Int 1; Tokeniser.Add]
+            Expected = Error ("Expected number or '('")
+        }
+        {
+            Name = "Testing error: Missing a number or a bracket: +1."
+            Args = [Tokeniser.Add; Tokeniser.Int 1]
+            Expected = Error ("Expected number or '('")
+        }
+        {
+            Name = "Testing error: Missing a closing bracket: (1+1."
+            Args = [Tokeniser.LeftBracket; Tokeniser.Int 1; Tokeniser.Add; Tokeniser.Int 1]
+            Expected = Error ("Missing closing bracket")
+        }
+
+    ]
+
+    [<TestCaseSource("AstParserTestCases")>]
+    // Unit test AST Parser.
+    member this.Test_ASTParser_HappyPaths(tc: ASTParserTestCase) =
+        // --------
+        // ASSEMBLE
+        // --------
+        let args = tc.Args
+        let expected = tc.Expected
+
+
+        // ---
+        // ACT
+        // ---
+        let actual = parse args
+
+        // ------
+        // ASSERT
+        // ------
+        Assert.That(actual, Is.EqualTo(expected))
---
+++ engine/ASTParser.fs
@@ -0,0 +1,66 @@
+﻿namespace Engine
+    // Grammar:
+    // <E>    ::= <T> <Eopt>
+    // <Eopt> ::= + <T> <Eopt> | - <T> <Eopt> | <empty>
+    // <T>    ::= <NR> <Topt>
+    // <Topt> ::= * <NR> <Topt> | / <NR> <Topt> | <empty>
+    // <NR>   ::= <int> | <float> | (E)
+    module ASTParser =
+        open Types
+        open Tokeniser
+
+        /// Parses a Number or a Parenthesis expression (<NR>).
+        let rec parseNumber(tokens : Token list) : Result<(Node * Token list), string> =
+            match tokens with
+            | Tokeniser.Int     intNumber   :: remainingTokens -> Ok (Number (NumType.Int    intNumber), remainingTokens)
+            | Tokeniser.Float   floatNumber :: remainingTokens -> Ok (Number (NumType.Float  floatNumber), remainingTokens)
+            | LeftBracket         :: remainingTokens ->
+                match parseExpression remainingTokens with
+                | Ok (expr, RightBracket :: remainingTokens)    -> Ok (ParenthesisExpression expr, remainingTokens)
+                | Ok (_)                                        -> Error "Missing closing bracket"
+                | Error err                                     -> Error err
+            | _                           -> Error "Expected number or '('"
+
+        /// Parses the Term <T>, which is a Number or Parenthesis Expression followed by optional * or / operations
+        and parseTerm(tokens : Token list) : Result<(Node * Token list), string> =
+            match parseNumber tokens with
+            | Ok result  -> parseTermOperators result
+            | Error err  -> Error err
+
+        /// Parses the multiplication and division operators in a term <Topt>.
+        and parseTermOperators(term, tokens : Token list) : Result<(Node * Token list), string> =
+            match tokens with
+            | Multiply :: remainingTokens ->
+                match parseNumber remainingTokens with
+                | Ok (nextTerm, remainingTokens)  -> parseTermOperators (BinaryOperation ("*", term, nextTerm), remainingTokens)
+                | Error err                 -> Error err
+            | Divide :: remainingTokens ->
+                match parseNumber remainingTokens with
+                | Ok (nextTerm, remainingTokens)  -> parseTermOperators (BinaryOperation ("/", term, nextTerm), remainingTokens)
+                | Error err                 -> Error err
+            | _ -> Ok (term, tokens)
+
+        /// Parses an Expression <E>, which can include + or - operators.
+        and parseExpression(tokens : Token list) : Result<(Node * Token list), string> =
+            match parseTerm tokens with
+            | Ok    result  -> parseExpressionOperators result
+            | Error err     -> Error err
+
+        /// Parses the addition and subtraction operators in an expression <Eopt>.
+        and parseExpressionOperators(expr, tokens : Token list) : Result<(Node * Token list), string> =
+            match tokens with
+            | Add :: remainingTokens ->
+                match parseTerm remainingTokens with
+                | Ok (t, remainingTokens)   -> parseExpressionOperators (BinaryOperation ("+", expr, t), remainingTokens)
+                | Error err                 -> Error err
+            | Minus :: remainingTokens ->
+                match parseTerm remainingTokens with
+                | Ok (t, remainingTokens)   -> parseExpressionOperators (BinaryOperation ("-", expr, t), remainingTokens)
+                | Error err                 -> Error err
+            | _ -> Ok (expr, tokens)
+
+        // Parse tokens.
+        let parse(tokens : Token list) : Result<Node, string> =
+            match parseExpression tokens with
+            | Ok (ast, _)   -> Ok ast
+            | Error err     -> Error err
