31d5b75647f6ed1b0c9553eab05a25c55c83554d
IgorSteps <igorstepanenko696@gmail.com>
2023-12-25 02:28:21 +0300

    Move some functionality to Model

--- app/internal/Models/Help/HelpModel.cs
+++ app/internal/Models/Help/HelpModel.cs
@@ -8,11 +8,21 @@ namespace app
 {
     public class HelpContentModel
     {
-        public string HelpText { get; set; }
+        public string ExpressionsHelpText { get; }
+        public string PlotHelpText { get; }
 
-        public HelpContentModel(string helpText)
+        public HelpContentModel()
         {
-            HelpText = helpText;
+            ExpressionsHelpText =
+                "Supported tokens are +, -, *, /, ), (, ^, =, sin, cos, tan, log, variable names like 'x' and 'y'\n" +
+                "You can assign variables like so x=5\n" +
+                "You can have multiple lines, just put a semicolon ';' on the end of your lines";
+
+            PlotHelpText =
+                "Able to plot linear and polynomial equations\n" +
+                "You can plot multiple graphs, just enter new equation and click Plot\n" +
+                "Click clear to clear plotting area\n" +
+                "Don't include 'y = ' part, otherwise we will error";
         }
     }
 }
--- app/internal/ViewModels/Help/HelpViewModel.cs
+++ app/internal/ViewModels/Help/HelpViewModel.cs
@@ -7,24 +7,14 @@ namespace app
     public class HelpViewModel : ObservableObject
     {
 
-        public string ExpressionsHelpText { get; set; }
-        public string PlotHelpText { get; set; }
+        private HelpContentModel _helpTextModel;
 
+        public HelpContentModel HelpTexts => _helpTextModel;
 
         public HelpViewModel()
         {
-            ExpressionsHelpText = 
-                "Supported tokens are +, -, *, /, ), (, ^, =, sin, cos, tan, log, variable names like 'x' and 'y'\n" +
-                "You can assign variables like so x=5\n" +
-                "You can have multiple lines, just put a semicolon ';' on the end of your lines";
-
-            PlotHelpText = 
-                "Able to plot linear and polynomial equations\n" +
-                "You can plot multiple graphs, just enter new equation and click Plot\n" +
-                "Click clear to clear plotting area\n" +
-                "Don't include 'y = ' part, otherwise we will error";
+            _helpTextModel = new HelpContentModel();
         }
 
-  
     }
 }
