6b5331b9499ffe2a0066a8bc7729d9710569c083
IgorSteps <igorstepanenko696@gmail.com>
2023-12-28 03:16:11 +0300

    Unit tests pt. 3

--- app.Test/Unit/Managers/PlotManager_test.cs
+++ app.Test/Unit/Managers/PlotManager_test.cs
@@ -1,12 +1,94 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Moq;
 
-namespace app.Test.Unit.Managers
+namespace app.Test.Unit
 {
-    internal class PlotManager_test
+    public class PlotManager_test
     {
+        private Mock<IFunctionEvaluator> _evaluator;
+        private PlotManager _manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            _evaluator = new Mock<IFunctionEvaluator>();
+            _manager = new PlotManager( _evaluator.Object );
+        }
+
+        [Test]
+        public void Test_PlotManager_CreatePlot_Success()
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            string function = "x^2";
+            double xmin = 0, xmax = 10, xstep = 1;
+
+            // --------
+            // ACT
+            // --------
+            var plot = _manager.CreatePlot(function, xmin, xmax, xstep);
+
+            // --------
+            // ASSERT
+            // --------
+            Assert.IsNotNull(plot, "Plot must not be null");
+            Assert.That(plot.Function, Is.EqualTo(function), "Plot's function doesn't match");
+            Assert.That(plot.XMin, Is.EqualTo(xmin), "Plot's xmin doesn't match");
+            Assert.That(plot.XMax, Is.EqualTo(xmax), "Plot's xmax doesn't match");
+            Assert.That(plot.XStep, Is.EqualTo(xstep), "Plot's xstep doesn't match");
+        }
+
+        [Test]
+        public void Test_PlotManager_GetLineSeriesForPlot_Success()
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            Plot plot = new Plot("x^2", 0, 10, 1);
+            var evaluationResult = new EvaluationResult(new[] { new double[] { 0, 0 }, new double[] { 1, 1 } }, null);
+            _evaluator.
+                Setup(f => f.Evaluate(plot.Function, plot.XMin, plot.XMax, plot.XStep)).
+                Returns(evaluationResult);
+
+            // --------
+            // ACT
+            // --------
+            var result = _manager.GetLineSeriesForPlot(plot);
+
+            // --------
+            // ASSERT
+            // --------
+            Assert.IsFalse(result.HasError, "HasError must be false");
+            Assert.IsNotNull(result.LineSeries, "Line series must not be null");
+            Assert.That(result.LineSeries.Points.Count, Is.EqualTo(2), "Line series should have 2 sets of [x,y] coords");
+            _evaluator.VerifyAll();
+        }
+
+        [Test]
+        public void Test_PlotManager_GetLineSeriesForPlot_Error()
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            Plot plot = new Plot("x^2", 0, 10, 1);
+            Error error = new Error("Error evaluating function");
+            var evaluationResult = new EvaluationResult(null, error);
+            _evaluator.
+                Setup(f => f.Evaluate(plot.Function, plot.XMin, plot.XMax, plot.XStep)).
+                Returns(evaluationResult);
+
+            // --------
+            // ACT
+            // --------
+            var result = _manager.GetLineSeriesForPlot(plot);
+
+            // --------
+            // ASSERT
+            // --------
+            Assert.IsTrue(result.HasError, "HasError must be true");
+            Assert.That(result.Error, Is.EqualTo(error), "Errors don't match");
+            Assert.IsNull(result.LineSeries, "Line series must be null");
+            _evaluator.VerifyAll();
+        }
     }
 }
