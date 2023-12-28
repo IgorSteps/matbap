9f640c344919ce5047402ac373dfc3a80e33de02
IgorSteps <igorstepanenko696@gmail.com>
2023-12-29 01:46:44 +0300

    Revert "ExpressionViewModel"
    This reverts commit 7cd66910242a047ad28c70dbd854c55affe07cc4.

--- app/internal/ViewModels/ViewModelLocator.cs
+++ app/internal/ViewModels/ViewModelLocator.cs
@@ -5,7 +5,7 @@ namespace app
 {
     public class ViewModelLocator
     {
-        public ExpressionViewModel ExpressionViewModel => App.Current.Services.GetService<ExpressionViewModel>();
+        public InterpretationViewModel InterpretationViewModel => App.Current.Services.GetService<InterpretationViewModel>();
         public HelpViewModel HelpViewModel => App.Current.Services.GetService<HelpViewModel>();
         public PlotViewModel PlotViewModel => App.Current.Services.GetService<PlotViewModel>();
         public ASTViewModel ASTViewModel => App.Current.Services.GetService<ASTViewModel>();
