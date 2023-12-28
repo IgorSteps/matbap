3abba74e35a239a85dac4e9b689a9b0a3a9469b4
IgorSteps <igorstepanenko696@gmail.com>
2023-12-29 01:39:56 +0300

    New interfces and naming clarification

---
+++ app/internal/Interfaces/IEvaluator.cs
@@ -0,0 +1,10 @@
+﻿namespace app
+{
+    /// <summary>
+    /// Interface for a serice to evaluate mathematical expressions.
+    /// </summary>
+    public interface IEvaluator
+    {
+        public ExpressionEvaluatingServiceResult Evaluate(string input);
+    }
+}
---
+++ app/internal/Interfaces/IFSharpFunctionEvaluatorWrapper.cs
@@ -0,0 +1,12 @@
+﻿namespace app
+{
+    /// <summary>
+    /// Interface for wrapping F# engine's function evaluation, and other mathematical operations.
+    /// </summary>
+    public interface IFSharpFunctionEvaluatorWrapper
+    {
+        public FunctionEvaluationResult Evaluate(string equation, double min, double max, double step);
+        public FunctionEvaluationResult EvaluateAtPoint(double x, string function);
+        public double TakeDerivative(double x, string function);
+    }
+}
