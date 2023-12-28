4b4f64ff9792c821216d4b4b2b75731fb547e12d
IgorSteps <igorstepanenko696@gmail.com>
2023-12-28 15:49:41 +0300

    Functional tests

---
+++ app.Test/Functional/EvaluateExpressionErrors.cs
@@ -0,0 +1,36 @@
+﻿namespace app.Test.Functional
+{
+    public class EvaluateExpressionErrorsTest
+    {
+        /// <summary>
+        /// Test to simualte user entering math expression that causes errors in Lexer and Parser.
+        /// </summary>
+        [Test]
+        [TestCase("2..0", "Invalid Float at token position 1: the mantissa cannot lead with non digit")]
+        [TestCase("2.0.0", "Invalid Float at token position 1: Can't have 2 decimal places in a float")]
+        [TestCase("@", "Invalid Token at token position 1: @")]
+        [TestCase("2++", "Error while parsing: Unexpected token or end of expression")]
+        [TestCase("2/0", "Error while parsing: division by 0")]
+        [TestCase("2.0 % 2", "Error while parsing: modulo cannot be used with floats")]
+        public void Test_EvaluateExpression_Error(string input, string expectedError)
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            var interpreter = new InterpretationModel();
+            var viewModel = new InterpretationViewModel(interpreter);
+            viewModel.Expression = input;
+            
+            // ---
+            // ACT
+            // ---
+            viewModel.InterpretCmd.Execute(null);
+
+
+            // ------
+            // ASSERT
+            // ------
+            Assert.That(viewModel.Response, Is.EqualTo(expectedError), "Errors don't match");
+        }
+    }
+}
---
+++ app.Test/Functional/EvaluateExpressionWithVariable.cs
@@ -0,0 +1,100 @@
+﻿namespace app.Test.Functional
+{
+    public class EvaluateExpressionWithVariable
+    {
+        /// <summary>
+        /// Test to simualte user entering math expression with variables.
+        /// </summary>
+        [Test]
+        public void Test_EvaluateExpression_WithVariable()
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            var interpreter = new InterpretationModel();
+            var viewModel = new InterpretationViewModel(interpreter);
+            viewModel.Expression = "x=5";
+            string nextExpression = "5+x";
+
+            // ---
+            // ACT
+            // ---
+            viewModel.InterpretCmd.Execute(null);
+
+            viewModel.Expression = nextExpression;
+            viewModel.InterpretCmd.Execute(null);
+
+
+            // ------
+            // ASSERT
+            // ------
+            Assert.That(viewModel.Response, Is.EqualTo("10"), "Responses don't match");
+        }
+
+        /// <summary>
+        /// Test to simualte user entering math expression with 2 variables.
+        /// </summary>
+        [Test]
+        public void Test_EvaluateExpression_WithMultipleVariables()
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            var interpreter = new InterpretationModel();
+            var viewModel = new InterpretationViewModel(interpreter);
+            viewModel.Expression = "x=5";
+            string nextVariable = "y=5";
+            string nextExpression = "5+x";
+
+            // ---
+            // ACT
+            // ---
+            viewModel.InterpretCmd.Execute(null);
+
+            viewModel.Expression = nextVariable;
+            viewModel.InterpretCmd.Execute(null);
+
+            viewModel.Expression = nextExpression;
+            viewModel.InterpretCmd.Execute(null);
+
+
+            // ------
+            // ASSERT
+            // ------
+            Assert.That(viewModel.Response, Is.EqualTo("10"), "Responses don't match");
+        }
+
+        /// <summary>
+        /// Test to simualte user entering math expression and reassinging variables.
+        /// </summary>
+        [Test]
+        public void Test_EvaluateExpression_ReassignVariables()
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            var interpreter = new InterpretationModel();
+            var viewModel = new InterpretationViewModel(interpreter);
+            viewModel.Expression = "x=5";
+            string nextVariable = "x=10";
+            string nextExpression = "5+x";
+
+            // ---
+            // ACT
+            // ---
+            viewModel.InterpretCmd.Execute(null);
+
+            viewModel.Expression = nextVariable;
+            viewModel.InterpretCmd.Execute(null);
+
+            viewModel.Expression = nextExpression;
+            viewModel.InterpretCmd.Execute(null);
+
+
+            // ------
+            // ASSERT
+            // ------
+            Assert.That(viewModel.Response, Is.EqualTo("15"), "Responses don't match");
+        }
+    }
+}
