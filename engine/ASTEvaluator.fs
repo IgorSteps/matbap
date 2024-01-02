ddb7a46a401264f4094f9dca321d09142a5e2315
Lyra <118722990+lyra-scarlet@users.noreply.github.com>
2024-01-02 15:40:41 +0000

    Skeleton for AST evaluator

---
+++ engine/ASTEvaluator.fs
@@ -0,0 +1,17 @@
+﻿namespace Engine
+    module ASTEvaluator =
+        let private getStrFromLexerError(err : Tokeniser.LexicalError) =
+            match err with
+                | Tokeniser.InvalidFloat str -> str
+                | Tokeniser.InvalidToken str -> str
+
+        let private parse(tokens: Tokeniser.Token list) =
+            match ASTParser.parse tokens with
+            | Error parseError  -> Error parseError
+            | Ok result         -> Ok result
+
+        let eval(exp : string) =
+            match Tokeniser.tokenise exp with
+            // Must return a result to C# app as the return type is different whether it fails or succeeds.
+            | Ok tokens -> parse tokens
+            | Error e -> Error (getStrFromLexerError e)
