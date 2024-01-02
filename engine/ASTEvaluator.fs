8836566a60827f1caf27779f3d356a5403bdd07a
Lyra <118722990+lyra-scarlet@users.noreply.github.com>
2024-01-02 16:09:42 +0000

    Improved skeleton

--- engine/ASTEvaluator.fs
+++ engine/ASTEvaluator.fs
@@ -1,5 +1,16 @@
 ﻿namespace Engine
     module ASTEvaluator =
+        // Functions for evaluating
+        let rec private evalTree (topNode : Types.Node) : Types.Node =
+            match topNode with
+            | Types.BinaryOperation (op,a,b) -> evalTree a
+            | Types.ParenthesisExpression _  -> Types.Number (Types.Float 0)
+            | Types.UnaryMinusOperation  _   -> Types.Number (Types.Float 0)
+            | Types.VariableAssignment  _    -> Types.Number (Types.Float 0)
+            | Types.Number n                 -> Types.Number n
+            // The above could also be written as _ -> _, but is more clear this way
+        
+        // Functions for handling evaluation
         let private getStrFromLexerError(err : Tokeniser.LexicalError) =
             match err with
                 | Tokeniser.InvalidFloat str -> str
@@ -12,6 +23,7 @@
 
         let eval(exp : string) =
             match Tokeniser.tokenise exp with
-            // Must return a result to C# app as the return type is different whether it fails or succeeds.
-            | Ok tokens -> parse tokens
+            | Ok tokens -> match parse tokens with
+                           | Ok tree -> Ok (evalTree tree)
+                           | Error e -> Error e
             | Error e -> Error (getStrFromLexerError e)
