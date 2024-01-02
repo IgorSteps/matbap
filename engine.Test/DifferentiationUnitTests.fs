25e92836b135bc4a669b3c00192e891195f139f4
Igor Stepanenko <igorstepanenko696@gmail.com>
2024-01-02 12:44:44 +0300

    Add errors messages

---
+++ engine.Test/DifferentiationUnitTests.fs
@@ -0,0 +1,93 @@
+﻿namespace Engine.Test
+
+module DifferentiationUnitTest =
+    open NUnit.Framework
+    open Engine
+    open Types
+    open Differentiation
+
+    type DifferentiationTestCase = {
+        Name: string;
+        Args: Node;
+        Expected: Node;
+    }
+
+    [<TestFixture>]
+    type DifferentiationTests() =
+        static member DifferentiationTestCases: DifferentiationTestCase list = [
+            {
+                Name = "Testing number differentiation"
+                Args = Number(Int 5)
+                Expected = Number(Int 0)
+            }
+            {
+                Name = "Testing variable differentiation"
+                Args = Variable("x")
+                Expected = Number(Int 1)
+            }
+            {
+                Name = "Testing polynomial differentiation"
+                Args = BinaryOperation("+", Variable("x"), Variable("y"))
+                Expected = BinaryOperation("+", Number(Int 1), Number(Int 0))
+            }
+            {
+                Name = "Testing product rule, x*y = y"
+                Args = BinaryOperation("*", Variable("x"), Variable("y"))
+                Expected = BinaryOperation(
+                                "+",
+                                BinaryOperation(
+                                    "*",
+                                    Number(Int 1),
+                                    Variable "y"
+                                ),
+                                BinaryOperation(
+                                    "*",
+                                    Variable "x",
+                                    Number(Int 0)
+                                )
+                            )
+            }
+            {
+                Name = "Testing quotent rule, x/y = ..."
+                Args = BinaryOperation("/", Variable("x"), Variable("y"))
+                Expected = BinaryOperation(
+                                "/",
+                                BinaryOperation(
+                                    "-",
+                                    BinaryOperation("*", Number(Int 1), Variable "y"),
+                                    BinaryOperation("*", Variable "x", Number(Int 0))
+                                ),
+                                BinaryOperation(
+                                    "^",
+                                    Variable "y",
+                                    Number(Int 2)
+                                )
+                            )
+            }
+            {
+                Name = "Testing power rule, x^2 = 2x"
+                Args = BinaryOperation("^", Variable("x"), Number(Int 2))
+                Expected = BinaryOperation("*", Number(Int 2), BinaryOperation("^", Variable "x", Number(Int 1)))
+            }
+        ]
+
+        [<TestCaseSource("DifferentiationTestCases")>]
+        member this.Test_Differentiation_Happy_Paths(tc: DifferentiationTestCase) =
+            // --------
+            // ASSEMBLE
+            // --------
+            let args = tc.Args
+            let expected = tc.Expected
+
+
+            // ---
+            // ACT
+            // ---
+            let actual = differentiate args "x"
+
+            // ------
+            // ASSERT
+            // ------
+            match actual with
+                   | Ok ast -> Assert.AreEqual(expected, ast)
+                   | Error err -> Assert.Fail("Parsing failed with unexpected error: " + err)
---
+++ engine/Differentiation.fs
@@ -0,0 +1,67 @@
+﻿namespace Engine
+    module Differentiation =
+        open Types
+        
+        /// Look Up Table for trigonometric or logarithmic functions.
+        let derivativeLUT = dict [
+            "sin", (fun x -> Function("cos", x))
+            "cos", (fun x -> UnaryMinusOperation("-", Function("sin", x)))
+            "tan", (fun x -> BinaryOperation("^", Function("sec", x), Number(Int 2)))
+            "log", (fun x -> BinaryOperation("/", Number(Int 1), x))
+        ]
+        
+        /// Differentiate with respect to a variable.
+        let rec differentiate (node: Node) (var: string) : Result<Node, string> =
+            match node with
+            | Number _ -> Ok(Number(Int 0))
+            | Variable (varName) -> 
+                if varName = var then 
+                    Ok(Number(Int 1))    
+                else 
+                    Ok(Number(Int 0))    
+            | BinaryOperation(operation, left, right) ->
+                match operation, differentiate left var, differentiate right var with
+                // d(u+v)/dx = du/dx + dv/dx
+                | "+", Ok(dLeft), Ok(dRight) -> Ok(BinaryOperation("+", dLeft, dRight))
+                // d(u-v)/dx = du/dx - dv/dx
+                | "-", Ok(dLeft), Ok(dRight) -> Ok(BinaryOperation("-", dLeft, dRight))
+                // d(u*v)/dx = u*dv/dx + v*du/dx
+                | "*", Ok(dLeft), Ok(dRight) -> Ok(BinaryOperation(
+                                                        "+", 
+                                                        BinaryOperation("*", dLeft, right),
+                                                        BinaryOperation("*", left, dRight)
+                                                        )
+                                                        )
+                // d(u/v)/dx = (v*du/dx - u*dv/dx) / v^2
+                | "/", Ok(dLeft), Ok(dRight) -> Ok(BinaryOperation(
+                                                        "/",
+                                                        BinaryOperation(
+                                                            "-",
+                                                            BinaryOperation("*", dLeft, right),
+                                                            BinaryOperation("*", left, dRight)
+                                                        ), 
+                                                        BinaryOperation("^", right, Number(Int 2))))
+                         
+                | "^", _, _ ->
+                    match left, right with
+                        | baseDiff, Number(Int power) ->
+                            let newPower = power - 1
+                            let newPowerExpr = BinaryOperation("^", baseDiff,  Number (Int newPower))
+                            let derivative = BinaryOperation("*", Number (Int power), newPowerExpr)
+                            Ok(derivative)
+                        | baseDiff, Number(Float power) ->
+                            let newPower = power - 1.0
+                            let newPowerExpr = BinaryOperation("^", baseDiff,  Number (Float newPower))
+                            let derivative = BinaryOperation("*", Number (Float power), newPowerExpr)
+                            Ok(derivative)
+                        | _, _ -> Error("Differentiation with non-constant power is not supported")
+                | _ -> Error(sprintf "Operation '%s' is not supported for differentiation" operation)
+            | ParenthesisExpression expression ->
+                match differentiate expression var with
+                | Ok(dExp)      -> Ok(ParenthesisExpression dExp)
+                | Error(msg)    -> Error(msg)
+            | UnaryMinusOperation (_, expression) ->
+                match differentiate expression var with
+                | Ok(dExp)      -> Ok(UnaryMinusOperation("-", dExp))
+                | Error(msg)    -> Error(msg)
+            | _ -> Error ("Unsupported node type for differentiation")
