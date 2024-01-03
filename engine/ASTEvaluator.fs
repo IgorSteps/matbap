a159b2211b7985ff3ab44f5e1805b1e2de567551
Lyra <118722990+lyra-scarlet@users.noreply.github.com>
2024-01-03 15:59:10 +0000

    Evaluator support for variables (no assignment)

--- engine/ASTEvaluator.fs
+++ engine/ASTEvaluator.fs
@@ -1,38 +1,56 @@
 ﻿namespace Engine
     module ASTEvaluator =
         open Types
+        open System.Collections.Generic
+        
+        type SymbolTable = Dictionary<string, NumType>
         // Functions for evaluating
-        let rec private evalTree (topNode : Node) : Result<Node, string> =
+        let rec private startEvalTree (topNode : Node) (symTable : SymbolTable) : Result<Node*SymbolTable, string> =
             match topNode with
-            | BinaryOperation (op, a, b)     -> match evalBinaryOperation (op, a, b) with
-                                                | Ok node -> Ok node
-                                                | Error e -> Error e
-            | ParenthesisExpression node     -> match evalTree node with
-                                                | Ok node -> Ok node
-                                                | Error e -> Error e
+            | VariableAssignment (varName, innerNode) -> setVar varName innerNode symTable
+            | _ -> match evalTree topNode symTable with
+                   | Ok node -> Ok (node, symTable)
+                   | Error e -> Error e
+            
+        and private setVar (varName : string) (topNode : Node) (symTable : SymbolTable) : Result<Node*SymbolTable, string> =
+            match evalTree topNode symTable with
+            | Ok _ -> Error "Variable assignment not yet supported"
+            | Error e -> Error e
+            
+        and private evalTree (node : Node) (symTable : SymbolTable) : Result<Node, string> =
+            match node with
+            | BinaryOperation (op, a, b)      -> match evalBinaryOp (op, a, b) symTable with
+                                                 | Ok node -> Ok node
+                                                 | Error e -> Error e
+            | ParenthesisExpression innerNode -> match evalTree innerNode symTable with
+                                                 | Ok node -> Ok node
+                                                 | Error e -> Error e
             // Not going to bother checking operator for unary minus operation, assume it's there for future extension?
-            | UnaryMinusOperation (_, num)   -> match evalNum num with
-                                                | Ok (Int x)   -> Ok (Number (Int -x))
-                                                | Ok (Float x) -> Ok (Number (Float -x))
-                                                | Error e      -> Error e
-            | VariableAssignment  _    -> Ok (Number (Float 0))
-            | Number n                 -> Ok (Number n)
+            | UnaryMinusOperation (_, num)    -> match evalNum num symTable with
+                                                 | Ok (Int x)   -> Ok (Number (Int -x))
+                                                 | Ok (Float x) -> Ok (Number (Float -x))
+                                                 | Error e      -> Error e
+            | Number n                        -> Ok (Number n)
+            | Variable varName                -> match symTable.ContainsKey varName with
+                                                 | true  -> Ok (Number symTable[varName])
+                                                 | false -> Error "Variable identifier not found."
+            | _ -> Error "Found an unexpected node in the syntax tree."
          
-        and private evalNum (node : Node) : Result<NumType, string> =
+        and private evalNum (node : Node) (symTable : SymbolTable) : Result<NumType, string> =
             // Helper function to eval a node and extract a number
-            match evalTree node with
+            match evalTree node symTable with
             | Ok node -> match node with
                          | Number num -> Ok num
                          | _ -> Error "Evaluation result wasn't a number!"
             | Error e -> Error e
             // These errors won't be seen normally since the function that calls this doesn't check the string
          
-        and private evalBinaryOperation (op : string, a : Node, b : Node) : Result<Node, string> =
+        and private evalBinaryOp (op : string, a : Node, b : Node) (symTable : SymbolTable) : Result<Node, string> =
             // This can probably be rewritten in a better way, but the NumType returned still needs to be different. We
             // could potentially test whether the value returned by the operator is either int or float, but that might
             // result in worse performance and the intent is arguably less clear. Any comments on this are appreciated.
-            let evalA = evalNum a
-            let evalB = evalNum b
+            let evalA = evalNum a symTable
+            let evalB = evalNum b symTable
             match op with
             | "+" -> match (evalA, evalB) with
                      | Ok (Int a),   Ok (Int b)   -> Ok (Number (Int   (a+b)))
@@ -80,24 +98,24 @@
             | _   -> Error "Unknown operator in AST evaluation."
         
         // Functions for handling evaluation
-        let private getStrFromLexerError(err : Tokeniser.LexicalError) =
+        let private getStrFromLexerError (err : Tokeniser.LexicalError) : string =
             match err with
                 | Tokeniser.InvalidFloat str -> str
                 | Tokeniser.InvalidToken str -> str
 
-        let private parse(tokens: Tokeniser.Token list) =
+        let private parse (tokens: Tokeniser.Token list) : Result<Node,string> =
             match ASTParser.parse tokens with
             | Error parseError  -> Error parseError
-            | Ok result         -> Ok result
+            | Ok tokens         -> Ok tokens
             
         let debug(exp : string) =
             match Tokeniser.tokenise exp with
             | Ok tokens -> Ok (parse tokens)
             | Error e -> Error e
 
-        let eval(exp : string) =
+        let eval (exp : string) (symTable : SymbolTable) =
             match Tokeniser.tokenise exp with
             | Ok tokens -> match parse tokens with
-                           | Ok tree -> evalTree tree
+                           | Ok tree -> evalTree tree symTable
                            | Error e -> Error e
             | Error e -> Error (getStrFromLexerError e)
