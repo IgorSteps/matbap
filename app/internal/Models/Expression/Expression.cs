0b9473fecd4ef54455de5c1fa0457ef45e28300a
IgorSteps <igorstepanenko696@gmail.com>
2024-01-03 15:16:14 +0300

    Add AST properties to Expression class

--- app/internal/Models/Expression/Expression.cs
+++ app/internal/Models/Expression/Expression.cs
@@ -1,8 +1,12 @@
-﻿namespace app
+﻿using FSharpAST = Engine.Types.Node;
+
+namespace app
 {
     public class Expression
     {
         public string Value { get; private set; }
+        public FSharpAST FSharpAST { get; private set; }
+        public ASTNode CSharpAST { get; private set; }
         public Expression(string expression)
         {
             Value = expression;
