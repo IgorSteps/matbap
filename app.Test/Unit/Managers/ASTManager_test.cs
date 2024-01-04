e90c4da93df669e151109f46161ddaebc66619b4
IgorSteps <igorstepanenko696@gmail.com>
2024-01-04 23:38:35 +0300

    Unit tests

--- app.Test/Unit/Managers/ASTManager_test.cs
+++ app.Test/Unit/Managers/ASTManager_test.cs
@@ -1,4 +1,6 @@
-﻿using FSharpASTNode = Engine.Types.Node;
+﻿using Microsoft.Msagl.Drawing;
+using Moq;
+using FSharpASTNode = Engine.Types.Node;
 using FSharpNumType = Engine.Types.NumType;
 
 namespace app.Test.Unit
@@ -136,6 +138,56 @@ namespace app.Test.Unit
 
             Assert.IsInstanceOf<NumberNode<int>>(cSharpBinaryNode.Right);
             Assert.That(((NumberNode<int>)cSharpBinaryNode.Right).Value, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void ASTManager_Convert_ASTtoGraph()
+        {
+            // --------
+            // ASSEMBLE
+            // --------
+            // Expected AST.
+            var rootAstNode = new NumberNode<int>(1);
+            var leftChild = new NumberNode<int>(2);
+            var rightChild = new NumberNode<int>(3);
+            rootAstNode.AddChild(leftChild);
+            rootAstNode.AddChild(rightChild);
+
+            // ---
+            // ACT
+            // ---
+            var graph = _converter.ConvertAstToGraph(rootAstNode);
+
+            // ------
+            // ASSERT
+            // ------
+            Assert.That(graph.NodeCount, Is.EqualTo(3), "Graph must have 3 nodes");
+            Assert.That(graph.EdgeCount, Is.EqualTo(2), "Graph must have 2 edges");
+
+            var rootNode = graph.FindNode(rootAstNode.Value.ToString());
+            Assert.That(rootNode, Is.Not.Null, "Root node can't be null");
+
+            // Check nodes.
+            var leftNode = graph.FindNode(leftChild.Value.ToString());
+            var rightNode = graph.FindNode(rightChild.Value.ToString());
+            Assert.That(leftNode, Is.Not.Null, "Left child can't be null");
+            Assert.That(rightNode, Is.Not.Null, "Right child can't be null");
+
+            // Check edges.
+            Assert.IsTrue(EdgeExists(graph, rootNode.Id, leftNode.Id), "Edge from root to left child must exist");
+            Assert.IsTrue(EdgeExists(graph, rootNode.Id, rightNode.Id), "Edge from root to right child must exist");
+        }
+
+        private bool EdgeExists(Graph graph, string sourceNodeId, string targetNodeId)
+        {
+            foreach (var edge in graph.Edges)
+            {
+                if (edge.SourceNode.Id == sourceNodeId && edge.TargetNode.Id == targetNodeId)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 }
