54cc2735d86e613003b5a7dec9be02ea635faf43
Lyra <118722990+lyra-scarlet@users.noreply.github.com>
2024-01-04 11:16:32 +0000

    Add plotPoints to new evaluator

--- engine/ASTEvaluator.fs
+++ engine/ASTEvaluator.fs
@@ -36,15 +36,15 @@
             | Number n                        -> Ok (Number n)
             | Variable varName                -> match symTable.ContainsKey varName with
                                                  | true  -> Ok (Number symTable[varName])
-                                                 | false -> Error "Variable identifier not found."
-            | _ -> Error "Found an unexpected node in the syntax tree."
+                                                 | false -> Error "Evaluation error: variable identifier not found."
+            | _ -> Error "Evaluation error: unexpected node found."
          
         and private evalNum (node : Node) (symTable : SymbolTable) : Result<NumType, string> =
             // Helper function to eval a node and then extract a number
             match evalTree node symTable with
             | Ok node -> match node with
                          | Number num -> Ok num
-                         | _ -> Error "Evaluation result wasn't a number!"
+                         | _ -> Error "Evaluation error: operation result wasn't a number!"
             | Error e -> Error e
          
         and private evalBinaryOp (op : string, a : Node, b : Node) (symTable : SymbolTable) : Result<Node, string> =
@@ -76,7 +76,11 @@
                      | Error e,      _            -> Error e
                      | _,            Error e      -> Error e
             | "/" -> match (evalA, evalB) with
-                     // Still needs a check for division by zero!
+                     // Easier to check for zero this way than to write a function to extract number from result
+                     | Ok (Int 0),     _ -> Error "Evaluation error: division by 0."
+                     | Ok (Float 0.0), _ -> Error "Evaluation error: division by 0."
+                     | _, Ok (Int 0)     -> Error "Evaluation error: division by 0."
+                     | _, Ok (Float 0.0) -> Error "Evaluation error: division by 0."
                      | Ok (Int a),   Ok (Int b)   -> Ok (Number (Int   (a/b)))
                      | Ok (Int a),   Ok (Float b) -> Ok (Number (Float (float a/b)))
                      | Ok (Float a), Ok (Int b)   -> Ok (Number (Float (a/float b)))
@@ -97,13 +101,17 @@
                      | Ok (Float a), Ok (Float b) -> Ok (Number (Float (a%b)))
                      | Error e,      _            -> Error e
                      | _,            Error e      -> Error e
-            | _   -> Error "Unknown operator in AST evaluation."
+            | _   -> Error "Evaluation error: unknown operator."
         
         // Helper functions for handling evaluation
         let private getStrFromLexerError (err : Tokeniser.LexicalError) : string =
             match err with
                 | Tokeniser.InvalidFloat str -> str
                 | Tokeniser.InvalidToken str -> str
+        let private getFloatFromNum (num : NumType) : float =
+            match num with
+                | Int x   -> float x
+                | Float x -> x
         let private parse (tokens: Tokeniser.Token list) : Result<Node,string> =
             match ASTParser.parse tokens with
             | Error parseError  -> Error parseError
@@ -118,3 +126,38 @@
                                         | Error e            -> Error e
                            | Error e -> Error e
             | Error e -> Error (getStrFromLexerError e)
+            
+        // Returns a list of points to plot based on a given minimum, maximum, and step. Step is forced to be positive,
+        // and min/max are treated as "start point" and "end point"
+        // Expression input in the form: y = <exp>
+        let plotPoints (min: float) (max: float) (step: float) (exp : string) : Result<float array array, string> =
+            // Definitions
+            let mutable points = ResizeArray<float array>()
+            let mutable x = float min
+            let mutable gotError = None
+            let trueStep = abs(step)
+            
+            // Create symbol table containing X
+            let symTable = SymbolTable()
+            symTable.Add("x", Float x)
+                
+            // Calculation loop. While within range of max
+            while (x <= max) do
+                // Set x and calculate
+                symTable["x"] <- Float x
+                let result = eval exp symTable
+                
+                match result with
+                | Ok ((_, y), _, _) -> points.Add([|x; getFloatFromNum y|])
+                // If we get an error, needs to be returned instead of the list of plots.
+                // gotError holds this and is checked once we leave the loop. X is set to max in order to break the loop
+                // NOTE: if it's division by zero, skips and does not plot the point, to plot equations such as 1/x
+                | Error e   ->  if e <> "Evaluation error: division by 0." then
+                                    gotError <- Some e
+                                    x <- max
+                // Increment x for loop
+                x <- x + trueStep
+                
+            match gotError with
+            | None -> Ok (points.ToArray())
+            | Some e -> Error e
