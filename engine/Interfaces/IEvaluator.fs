9056da5fd6d9dd2cf41fee858a938d75e4a985a5
IgorSteps <igorstepanenko696@gmail.com>
2024-01-05 01:16:25 +0300

    Add interfaces and wrapper to their folders. Change to call AST evaluator.

--- engine/Interfaces/IEvaluator.fs
+++ engine/Interfaces/IEvaluator.fs
@@ -1,10 +1,10 @@
 ﻿namespace Engine
     open System.Collections.Generic
-    type SymbolTable = Dictionary<string, Parser.NumType>
-        
+    open ASTEvaluator
+    open Types
     /// Interface for evaluating mathematical expressions and plot functions.
     type IEvaluator =
-        abstract member Eval: string * SymbolTable -> Result<(string * SymbolTable), string>
+        abstract member Eval: string * SymbolTable -> Result<(string * SymbolTable * Node), string>
         abstract member PlotPoints: float * float * float * string -> Result<float array array, string>
 
 
