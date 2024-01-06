a11a3b3b54f6f7a81b673d41aee800b0040e9b03
IgorSteps <igorstepanenko696@gmail.com>
2024-01-07 02:31:17 +0300

    Add fsharp wrapper

---
+++ app/internal/Models/Expression/FSharpFindRootsWrapper.cs
@@ -0,0 +1,22 @@
+﻿namespace app
+{
+    public struct FSharpFindRootResult
+    {
+        public string Answer { get; private set; }
+        public Error Error { get; private set; }
+        public readonly bool HasError => Error != null;
+        public FSharpFindRootResult(string answer, Error err)
+        {
+            Answer = answer;
+            Error = err;
+        }
+    }
+    public class FSharpFindRootsWrapper : IFSharpFindRootsWrapper
+    {
+        public FSharpFindRootResult FindRoots(string expression, double xmin, double xmax)
+        {
+            return new FSharpFindRootResult(null, null);
+        }
+
+    }
+}
