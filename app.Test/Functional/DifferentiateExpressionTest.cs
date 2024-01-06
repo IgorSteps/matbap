6fad49d5ef0ec29e872ce5a6c23bfa09e5749696
IgorSteps <igorstepanenko696@gmail.com>
2024-01-06 21:16:53 +0300

    Better functional tests

--- app.Test/Functional/DifferentiateExpressionTest.cs
+++ app.Test/Functional/DifferentiateExpressionTest.cs
@@ -13,25 +13,9 @@
             // --------
             
             var expression = "x^2";
-            
-            // F# wrappers.
-            Engine.EvaluatorWrapper evaluatorWrapper = new Engine.EvaluatorWrapper();
-            Engine.DifferentiatorWrapper differentiatorWrapper = new Engine.DifferentiatorWrapper();
-            Engine.ASTGetterWrapper astGetter = new Engine.ASTGetterWrapper();
-            
-            // C# wrappers.
-            var fsharpDifferentiatorWrapper = new FSharpDifferentiatorWrapper(differentiatorWrapper);
-            var fSharpASTGetterWrapper = new FSharpASTGetterWrapper(astGetter);
-            var evaluator = new FSharpEvaluatorWrapper(evaluatorWrapper);
 
-            var manager = new ExpressionManager(fsharpDifferentiatorWrapper);
-            var symTableManager = new SymbolTableManager();
-            var converter = new ASTManager();
-            var validator = new ValidationService();
+            var viewModel = Utils.CreateExpressionViewModel();
 
-            var service = new ExpressionEvaluatingService(fSharpASTGetterWrapper, validator, symTableManager, evaluator, manager, converter);
-            
-            var viewModel = new ExpressionViewModel(service);
             viewModel.Expression = expression;
 
             // ---
