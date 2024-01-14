c2d895820e175a694818c108458fd23f610dcfd7
IgorSteps <igorstepanenko696@gmail.com>
2024-01-14 16:17:43 +0000

    Functional test

--- app.Test/Functional/EvaluateExpressionWithVariable.cs
+++ app.Test/Functional/EvaluateExpressionWithVariable.cs
@@ -5,7 +5,7 @@ namespace app.Test.Functional
     public class EvaluateExpressionWithVariable
     {
         /// <summary>
-        /// Test to simualte user entering math expression with variables.
+        /// Test to simualte user entering math expression with variables, and GUI Symbol table showing them.
         /// </summary>
         [Test]
         public void Test_EvaluateExpression_WithVariable()
@@ -33,10 +33,13 @@ namespace app.Test.Functional
             // ASSERT
             // ------
             Assert.That(viewModel.Answer, Is.EqualTo("10"), "Responses don't match");
+            Assert.That(viewModel.GUISymbolTable.Count, Is.EqualTo(1), "GUI Symbol table must have an entry");
+            var kv = new KeyValuePair<string, Engine.Types.NumType>("x", Engine.Types.NumType.NewInt(5));
+            Assert.That(viewModel.GUISymbolTable.Contains(kv), "GUI Symbol table is missing a key value pair");
         }
 
         /// <summary>
-        /// Test to simualte user entering math expression with 2 variables.
+        /// Test to simualte user entering math expression with 2 variables, and GUI Smybol table reflecting it.
         /// </summary>
         [Test]
         public void Test_EvaluateExpression_WithMultipleVariables()
@@ -68,10 +71,15 @@ namespace app.Test.Functional
             // ASSERT
             // ------
             Assert.That(viewModel.Answer, Is.EqualTo("10"), "Responses don't match");
+            Assert.That(viewModel.GUISymbolTable.Count, Is.EqualTo(2), "GUI Symbol table must have 2 entries");
+            var kv = new KeyValuePair<string, Engine.Types.NumType>("x", Engine.Types.NumType.NewInt(5));
+            var kv2 = new KeyValuePair<string, Engine.Types.NumType>("y", Engine.Types.NumType.NewInt(5));
+            Assert.That(viewModel.GUISymbolTable.Contains(kv), "GUI Symbol table is missing a key value pair");
+            Assert.That(viewModel.GUISymbolTable.Contains(kv2), "GUI Symbol table is missing a key value pair");
         }
 
         /// <summary>
-        /// Test to simualte user entering math expression and reassinging variables.
+        /// Test to simualte user entering math expression and reassinging variables, and GUI Smybol table reflecting it.
         /// </summary>
         [Test]
         public void Test_EvaluateExpression_ReassignVariables()
@@ -103,6 +111,9 @@ namespace app.Test.Functional
             // ASSERT
             // ------
             Assert.That(viewModel.Answer, Is.EqualTo("15"), "Responses don't match");
+            Assert.That(viewModel.GUISymbolTable.Count, Is.EqualTo(1), "GUI Symbol table must have 1 entry");
+            var kv = new KeyValuePair<string, Engine.Types.NumType>("x", Engine.Types.NumType.NewInt(10));
+            Assert.That(viewModel.GUISymbolTable.Contains(kv), "GUI Symbol table is missing a key value pair");
         }
     }
 }
